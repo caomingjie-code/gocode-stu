64ad8d78147212e01866d2d5cc79a4cf2c7bb34e
caomingjie <caomingjie>
2021-11-24 13:26:30 +0800

    int

---
+++ basevar/IntStu.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+/**
+ * 研究 go int
+ */
+func main() {
+	var name = 9                                     //声明一定要用。否则会爆红
+	fmt.Println("name的值为：", name)                    //name的值为： 9
+	fmt.Println("name值的类型为： ", reflect.TypeOf(name)) //name值的类型为：  int
+
+	var name8 int8 = 9
+	fmt.Println("name8 的值为：", name8)
+	fmt.Println("name8 的类型为：", reflect.TypeOf(name8)) //name8 的类型为： int8
+	name8 = -9
+	fmt.Println("name8 的值为：", name8)                  //name8 的值为： -9
+	fmt.Println("name8 的类型为：", reflect.TypeOf(name8)) //name8 的类型为： int8
+	//name8 = 255; //此处会编译报错：constant 255 overflows int8
+	//fmt.Println("name8 的值为：",name8)
+	//fmt.Println("name8 的类型为：", reflect.TypeOf(name8))
+	/**
+	 * int 主要有 int, int8 int16 int 32 int64.
+	 */
+
+}
